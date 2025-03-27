4b8a50954fcdb2c72c29863dfc8b23c7a8c32559
chuck-w-moore <moore3cw@mail.uc.edu>
2025-03-27 11:52:08 -0400

    Add project files.

---
+++ mini_group_proj_IS3050_3_27_25/Kush.cs
@@ -0,0 +1,27 @@
+﻿/* Group 24 member 1: Chuck Moore
+ * 
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_group_proj_IS3050_3_27_25
+{
+    internal class Kush
+    {
+        private string _gatorade_flavor;
+        private int _sugar_amount;
+
+
+        public int Sugar_amount { get { return _sugar_amount; } set { _sugar_amount = value; } }
+        // Add the getter and setter for the gatorade property
+        public String Gatorade_flavor { get { return _gatorade_flavor; } set { _gatorade_flavor = value; } }
+
+    }
+}
---
+++ mini_group_proj_IS3050_3_27_25/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ 
+ 
+ 
+ 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_group_proj_IS3050_3_27_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kush drink = new Kush();  //Declare and instantiate the class
+
+            drink.Gatorade_flavor = "lemon lime"; //Invoke the setter
+            Console.WriteLine("The flavor of my gatorade is " + drink.Gatorade_flavor); // getter
+        }
+    }
+}
